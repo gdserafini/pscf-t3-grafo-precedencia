c5380c2575e44f6d0d1cb0732905842b5a3e3713
gdserafini <gdinizserafini@gmail.com>
2024-11-11 20:56:55 -0300

    updated code

--- main.py
+++ main.py
@@ -1,74 +1,56 @@
-
 """
 Equipe
 - Giordano Diniz Serafini
 - Victor Gabriel Eidt Bragagnolo
 """
+
 import multiprocessing as mp
 import time
-from multiprocessing.synchronize import Semaphore
 
-RANGE_COUNT = 5
-TIME_COUNT = 1
-
-
-def process(name: str, n: int, t: int,
-            actual_semaphore: Semaphore, next_semaphore: Semaphore) -> None:
-    print(f'Processo {name} aguardando')
-    actual_semaphore.acquire()
-    print(f'Processo {name} iniciado')
+def process(name: str, n: int, t: float,
+            start_semaphore: mp.Semaphore, end_semaphores: list = None) -> None:
+    print(f'Processo {name} aguardando\n')
+    start_semaphore.acquire()
+    print(f'Processo {name} iniciado\n')
     for i in range(1, n + 1):
-        print(f'{name} - {i}')
+        print(f'{name} - {i}\n')
         time.sleep(t)
-    print(f'Processo {name} finalizado')
-    next_semaphore.release()
-
+    print(f'Processo {name} finalizado\n')
+    if end_semaphores:
+        for sem in end_semaphores:
+            sem.release()
 
 def main() -> None:
-    range_count = int(input('Digite um valor inteiro para contagem: '))
-    time_count = int(input('Digite um valor para o tempo de contagem: '))
-
-    semaphore1 = mp.Semaphore(0)
-    semaphore2 = mp.Semaphore(0)
-    semaphore4 = mp.Semaphore(0)
-
-    process1 = mp.Process(
+    range_count = int(input('Digite um valor para contagem: '))
+    time_count = float(input('Digite um valor para a contagem: '))
+    semaphore_a = mp.Semaphore(1)
+    semaphore_b = mp.Semaphore(0)
+    semaphore_c = mp.Semaphore(0)
+    semaphore_d = mp.Semaphore(0)
+    process_a = mp.Process(
         target=process,
-        args=('Processo 1', range_count, time_count,
-              semaphore1, semaphore2
-              )
+        args=('Processo A', range_count, time_count, semaphore_a, [semaphore_b, semaphore_c])
     )
-    process2 = mp.Process(
+    process_b = mp.Process(
         target=process,
-        args=('Processo 2', range_count, time_count,
-              semaphore2, semaphore4
-              )
+        args=('Processo B', range_count, time_count, semaphore_b, [semaphore_d])
     )
-    process3 = mp.Process(
+    process_c = mp.Process(
         target=process,
-        args=('Processo 3', range_count, time_count,
-              semaphore2, semaphore4
-              )
+        args=('Processo C', range_count, time_count, semaphore_c, [semaphore_d])
     )
-    process4 = mp.Process(
+    process_d = mp.Process(
         target=process,
-        args=('Processo 4', range_count, time_count,
-              semaphore4, semaphore1
-              )
+        args=('Processo D', range_count, time_count, semaphore_d)
     )
-
-    process1.start()
-    process2.start()
-    process3.start()
-    process4.start()
-    time.sleep(TIME_COUNT)
-    semaphore1.release()
-    process1.join()
-    process2.join()
-    process3.join()
-    process4.join()
-
+    process_a.start()
+    process_b.start()
+    process_c.start()
+    process_d.start()
+    process_a.join()
+    process_b.join()
+    process_c.join()
+    process_d.join()
 
 if __name__ == '__main__':
     main()
-
